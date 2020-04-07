d819bca2acc5783dae3aa552f45049724ae62c51
Andrey <andrey.sviontek@gmail.com>
2020-04-07 22:13:32 +0500

    feat: add keys array

---
+++ main.js
@@ -0,0 +1,5 @@
+const keyArray = ['ё','1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','+','backspace',
+                'tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з','х','ъ','\'',
+                'capsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д','ж', 'э', 'enter',
+                'leftShift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', 'upArrow', 'rightShift',
+                'leftCtrl', 'alt', 'space', 'alt', 'rightCtrl', 'leftArrow','downArrow','rightArrow']
