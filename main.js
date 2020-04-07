33c03e9147f8948419c54b5cd231f2759282690d
Andrey <andrey.sviontek@gmail.com>
2020-04-07 23:24:30 +0500

    feat: add warning about work on task after deadline

--- main.js
+++ main.js
@@ -6,14 +6,19 @@ const keyArray = ['ё','1', '2', '3', '4', '5', '6', '7', '8', '9', '0','-','+',
 
 const section = document.createElement('div');
 const textArea = document.createElement('textarea');
-const keyboard = document.createElement('div');    
+const keyboard = document.createElement('div');   
+const warning = document.createElement('h1')
 
 section.classList.add('wrapper');
 document.body.appendChild(section);
 textArea.classList.add('input');
+section.appendChild(warning);
+warning.textContent = 'Работу я начал делать 06.04, буду доделывать уже после дэдлайна'
 section.appendChild(textArea);
 keyboard.classList.add('keyboard');
 section.appendChild(keyboard);
+
+
 
 function renderKeyboard(keyArray) {                                                     //render default keyboard
     for(let i = 0; i<keyArray.length; i++){
